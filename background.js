c3498054553d2fbf2c55cf9d31a5bf917523621b
Zack Nathan <zachary.nathan.22@dartmouth.edu>
2023-04-15 17:40:03 -0400

    add script to html

--- background.js
+++ background.js
@@ -1,13 +1,5 @@
 import { queryArxivAPI } from "./arxiv.js"
 
-chrome.action.onClicked.addListener(async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['content.js']
-    });
-});
-
 chrome.runtime.onMessage.addListener(
     function(message, sender, sendResponse) {
         console.log(message.content);
