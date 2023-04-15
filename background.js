f99c132d1aec6495ae459876f120c5c60e19a455
Zack Nathan <zachary.nathan.22@dartmouth.edu>
2023-04-15 13:17:36 -0400

    message listener

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
-chrome.action.onClicked.addListener(async (tab) => {
-    console.log('click');
+chrome.action.onClicked.addListener(async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         files: ['content.js']
     });
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    // Handle the message from the content script here
+    console.log('Received content:', message.content);
 });
--- content.js
+++ content.js
@@ -3,8 +3,6 @@ function readAndParseWebpageContent() {
     // For example, you can extract all headings from the webpage:
     const content = document.body.innerText;
 
-    console.log(content);
-
     // Send the parsed data to the background script or another part of the extension as needed.
     chrome.runtime.sendMessage({ content: content });
 }
