6e41f256b83fdc965c20333d483051685fe020cd
chmorris37 <charliemorris120@gmail.com>
2023-04-15 20:08:34 -0400

    dancing in the moonlight

--- background.js
+++ background.js
@@ -9,6 +9,25 @@ chrome.runtime.onMessage.addListener(
         if (message.type === "innertext") {
             console.log(message.text);
         }
+        
+        //Reformating text to work with the API
+        const data = {data: {
+            text: message.text
+        }};
+
+        fetch("http://zsg350.pythonanywhere.com/api/process", {
+            method: 'POST',
+            headers: {
+                'Content_Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then((listOfKeywords) => {
+            console.log(listofKeyWords);
+        }) .catch(error => console.error(error));
+
+    
+
+        
 
         // query the arxiv API using the keywords
         const papers = queryArxivAPI("Deep learning", 5);
