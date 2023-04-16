58327d94711bda00c09bfa9cfe770a28e3cba4ef
Zack Nathan <zachary.nathan.22@dartmouth.edu>
2023-04-15 22:30:00 -0400

    truncate innertext

--- innertext.js
+++ innertext.js
@@ -1,7 +1,7 @@
 // injected script to send document text to the background
 
 // remove extra whitespace and punctuation
-var text = document.body.innerText;
+var text = document.body.innerText.substring(0, 10000);
 var spaces = text.replace(/\s+/g, " ");
 var cleaned = spaces.replace(/[^a-zA-Z ]+/g, " ");
 
